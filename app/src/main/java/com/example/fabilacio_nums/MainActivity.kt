4daeac9124a13327df45341f0fce00c54ee9814f
EmmilyOdhiambo <emmilysodhiambo@gmail.com>
2023-05-24 19:56:59 +0300

    numbers

---
+++ app/src/main/java/com/example/fabilacio_nums/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fabilacio_nums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/fabilacio_nums/NumbersRvAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.fabilacio_nums
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class NumbersRvAdapter (var number:List<Int>):RecyclerView.Adapter<NumbersRvAdapter.NumbersViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NumbersRvAdapter.NumbersViewHolder {
+     val view =LayoutInflater.from(parent.context).inflate(R.layout.number_list_item,parent,false)
+        return NumbersViewHolder
+    }
+
+    override fun onBindViewHolder(holder: NumbersRvAdapter.NumbersViewHolder, position: Int) {
+        holder.tvNumber.text=numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+
+    }
+    class NumbersViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        var tvNumber=itemView.findViewById<TextView>(R.Id.tvNumbers)
+    }
+}
